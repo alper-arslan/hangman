d37ec7acda190d74510d2a528e5c3ddf4aed5feb
MagneticFields <36078894+MagneticFields@users.noreply.github.com>
2018-04-25 01:48:23 +0300

    Add files via upload

--- hangman_startover.py
+++ hangman_startover.py
@@ -18,6 +18,16 @@ def getAvailableLetters(lettersGuessed):
             availLetters_list.remove(temp)
     availLetters = "".join(availLetters_list)
     return availLetters
+
+def wordGuessed(guess):
+    
+    """
+    this checks the user input guess if it is entered before
+    """    
+    if guess not in lettersGuessed:
+        return False
+    else:
+        return True
 def hangman(secretWord):
     number_of_guesses = 8 # left guesses will be stored here
     print("Welcome to the game, Hangman!")
@@ -25,4 +35,13 @@ def hangman(secretWord):
     print("-------------")
     print("You have", str(number_of_guesses), "guesses left.")
     print("Available letters: ", getAvailableLetters(lettersGuessed))
+    while True:
+        guess_raw = input("Please guess a letter: ")
+        guess = guess_raw.lower()
+        if wordGuessed(guess) is False:
+            lettersGuessed.append(guess)
+        else:
+            guess_raw = input("Oops! You've already guessed that letter: ")
+            guess = guess_raw.lower()
+            
     
