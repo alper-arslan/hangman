8266296ad258126e0daf1e09f25966b725689dd8
MagneticFields <36078894+MagneticFields@users.noreply.github.com>
2018-04-24 23:55:38 +0300

    Add files via upload

--- hangman_startover.py
+++ hangman_startover.py
@@ -8,11 +8,21 @@ Created on Tue Apr 24 21:48:07 2018
 # for taking too many bearks from studying, I decided to start over the hangman
 # game
 lettersGuessed = [] # Guessed letters will be stored here
-def getAvailableLetters(lettersGuessed)
+def getAvailableLetters(lettersGuessed):
+    import string
+    availLetters = string.ascii_lowercase
+    availLetters_list = list(availLetters)
+    for i in range(len(lettersGuessed)):
+        if lettersGuessed[i] in availLetters_list:
+            temp = str(lettersGuessed[i])
+            availLetters_list.remove(temp)
+    availLetters = "".join(availLetters_list)
+    return availLetters
 def hangman(secretWord):
     number_of_guesses = 8 # left guesses will be stored here
     print("Welcome to the game, Hangman!")
     print("I am thinking of a word that is", len(secretWord), "letters long.")
     print("-------------")
     print("You have", str(number_of_guesses), "guesses left.")
+    print("Available letters: ", getAvailableLetters(lettersGuessed))
     
